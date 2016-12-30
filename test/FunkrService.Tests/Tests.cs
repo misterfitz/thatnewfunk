b6e1783932a471c35298369084a05d05826f1e0f
Ryan Fitzgerald <fitz297@gmail.com>
2016-12-29 22:48:56 -0500

    initial testing setup

---
+++ test/FunkrService.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsoleApplication;
+using Xunit;
+
+namespace Funkr.UnitTests.Servics
+{
+    public class FunkrService_ProcessEventData
+    {
+
+        private readonly ConsoleApplication.FunkrService _funkrService;
+
+        public FunkrService_ProcessEventData() 
+        {
+            _funkrService = new FunkrService();
+        }
+
+        [Fact]
+        public void ParseBasicEvent() 
+        {
+
+            Assert.True(false);
+        }
+    }
+}
